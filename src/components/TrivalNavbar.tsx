08155f1b23d88d83902e4933d3bcc41c141cc067
kallul <gogoikallul33@gmail.com>
2025-12-22 21:48:09 +0530

    noting changes

--- src/components/TrivalNavbar.tsx
+++ src/components/TrivalNavbar.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Image from "next/image";
 import { useRouter, usePathname } from "next/navigation";
-
+//just for see.. testing git
 interface NavItem {
   id: string;
   icon: string;
